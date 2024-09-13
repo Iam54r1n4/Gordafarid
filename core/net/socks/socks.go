dd48defc8e1937f66d7686869cfbe5d19e07d553
Sarina <iam54r1n40@proton.me>
2024-09-13 17:48:40 +0330

    Write socks5 protocol handshake & Update errors

---
+++ core/net/socks/socks.go
@@ -0,0 +1,137 @@
+package socks
+
+import (
+	"context"
+	"encoding/binary"
+	"errors"
+	"fmt"
+	"io"
+	"net"
+
+	"github.com/Iam54r1n4/Gordafrid/core/net/utils"
+	"github.com/Iam54r1n4/Gordafrid/internal/proxy_error"
+)
+
+// Constants for SOCKS5 protocol
+const (
+	// version represents the SOCKS protocol version (SOCKS5)
+	version = 5
+
+	// AtypIPv4 represents the address type for IPv4 addresses
+	AtypIPv4 = 1
+
+	// AtypIPv6 represents the address type for IPv6 addresses
+	AtypIPv6 = 4
+
+	// AtypDomain represents the address type for domain names
+	AtypDomain = 3
+)
+
+// HandshakeChan is used to communicate the result of the handshake
+type HandshakeChan struct {
+	TAddr string // Target address
+	ATyp  byte   // Address type
+	Err   error  // Error, if any
+}
+
+// Handshake performs the SOCKS5 handshake process
+func Handshake(ctx context.Context, c net.Conn, hChan chan<- HandshakeChan) {
+	defer close(hChan)
+
+	// Read the SOCKS version
+	buf := make([]byte, 2)
+	if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadVersion, err)}
+		return
+	}
+	// Return error if the version is not supported
+	if buf[0] != version {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnsupportedVersion, fmt.Errorf("sent version: %d", buf[0]))}
+		return
+	}
+
+	// Read the number of authentication methods
+	nMethods := buf[1]
+	// Discard the SOCKS5 authentication methods
+	if _, err := io.CopyN(io.Discard, c, int64(nMethods)); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToDiscardNMethods, fmt.Errorf("sent nmethods: %d", nMethods), err)}
+		return
+	}
+
+	// Send the SOCKS5 response (version and no authentication required)
+	if _, err := c.Write([]byte{version, 0}); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToSendVersionResponse, err)}
+		return
+	}
+
+	// Read the SOCKS5 request
+	buf = make([]byte, 3)
+	if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadRequest, err)}
+		return
+	}
+	if buf[0] != version || buf[1] != 1 {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnsupportedVersionOrCommand, fmt.Errorf("unsupported socks request:\nVersion: %d\nCommand: %d", buf[0], buf[1]))}
+		return
+	}
+
+	// Read the address type
+	if _, err := utils.ReadWithContext(ctx, c, buf[:1]); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadAddressType, err)}
+		return
+	}
+
+	var taddr string
+	var atyp byte = buf[0]
+	switch atyp {
+	case AtypIPv4: // IPv4
+		buf = make([]byte, net.IPv4len)
+		if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+			hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadIpv4, err)}
+			return
+		}
+		taddr = net.IP(buf).String()
+	case AtypIPv6: // IPv6
+		buf = make([]byte, net.IPv6len)
+		if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+			hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadIpv6, err)}
+			return
+		}
+		taddr = string(buf)
+	case AtypDomain: // Domain
+		if _, err := utils.ReadWithContext(ctx, c, buf[:1]); err != nil {
+			hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadDomain, err)}
+			return
+		}
+		buf = make([]byte, buf[0])
+		if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+			hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadDomain, err)}
+			return
+		}
+		taddr = string(buf)
+	default:
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnsupportedAddressType, fmt.Errorf("sent address type: %d", atyp))}
+		return
+	}
+
+	// Read the port
+	buf = make([]byte, 2)
+	if _, err := utils.ReadWithContext(ctx, c, buf); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToReadPort, err)}
+		return
+	}
+	var tport uint16 = binary.BigEndian.Uint16(buf)
+
+	// Combine the address and port into a single string
+	var fullTargetAddr string = net.JoinHostPort(taddr, fmt.Sprint(tport))
+
+	// Send the SOCKS5 response (success)
+	res := []byte{version, 0, 0, 1, 0, 0, 0, 0, 0, 0}
+	if _, err := c.Write(res); err != nil {
+		hChan <- HandshakeChan{Err: errors.Join(proxy_error.ErrSocks5UnableToSendSuccessResponse, err)}
+		return
+	}
+
+	// Send the successful handshake result
+	hChan <- HandshakeChan{TAddr: fullTargetAddr, ATyp: atyp}
+}
--- internal/proxy_error/proxy_error.go
+++ internal/proxy_error/proxy_error.go
@@ -3,19 +3,40 @@ package proxy_error
 import "errors"
 
 // Connection errors
-var ErrConnectionClosed error = errors.New("connection unexpectedly closed")
-var ErrConnectionAccepting error = errors.New("failed to accept incoming connection")
-var ErrSocks5HandshakeTimeout error = errors.New("SOCKS5 handshake timed out")
-var ErrSocks5HandshakeFailed error = errors.New("SOCKS5 handshake failed: protocol mismatch or authentication error")
+var (
+	ErrConnectionClosed    = errors.New("connection unexpectedly closed")
+	ErrConnectionAccepting = errors.New("failed to accept incoming connection")
+)
+
+// Socks5 errors
+var (
+	ErrSocks5HandshakeTimeout            = errors.New("SOCKS5 handshake timed out")
+	ErrSocks5HandshakeFailed             = errors.New("SOCKS5 handshake failed: protocol mismatch or authentication error")
+	ErrSocks5UnsupportedVersion          = errors.New("unsupported SOCKS5 version")
+	ErrSocks5UnableToReadVersion         = errors.New("unable to read SOCKS5 version")
+	ErrSocks5UnableToDiscardNMethods     = errors.New("unable to discard SOCKS5 nmethods")
+	ErrSocks5UnableToSendVersionResponse = errors.New("unable to send SOCKS5 version response(first response)") // First response
+	ErrSocks5UnableToReadRequest         = errors.New("unable to read SOCKS5 request")
+	ErrSocks5UnsupportedVersionOrCommand = errors.New("unsupported SOCKS5 version or command(in handshake request)")
+	ErrSocks5UnableToReadAddressType     = errors.New("unable to read address type")
+	ErrSocks5UnsupportedAddressType      = errors.New("unsupported SOCKS5 address type")
+	ErrSocks5UnableToReadIpv4            = errors.New("unable to read IPv4 address")
+	ErrSocks5UnableToReadIpv6            = errors.New("unable to read IPv6 address")
+	ErrSocks5UnableToReadDomain          = errors.New("unable to read domain name")
+	ErrSocks5UnableToReadPort            = errors.New("unable to read port")
+	ErrSocks5UnableToSendSuccessResponse = errors.New("unable to send success response(second response)") // Second response
+)
 
 // Listening errors
-var ErrServerListenFailed error = errors.New("server failed to start listening on specified address")
-var ErrClientListenFailed error = errors.New("client failed to start listening on specified address")
-var ErrServerDialFailed error = errors.New("server failed to establish connection with target")
-var ErrClientToServerDialFailed error = errors.New("client failed to establish connection with proxy server")
+var (
+	ErrServerListenFailed       = errors.New("server failed to start listening on specified address")
+	ErrClientListenFailed       = errors.New("client failed to start listening on specified address")
+	ErrServerDialFailed         = errors.New("server failed to establish connection with target")
+	ErrClientToServerDialFailed = errors.New("client failed to establish connection with proxy server")
+)
 
 // Copying errors
-var ErrTransferError error = errors.New("data transfer failed between client and server")
+var ErrTransferError = errors.New("data transfer failed between client and server")
 
 // Crypto errors
-var ErrChacha20poly1305Failed error = errors.New("encryption/decryption failed using ChaCha20-Poly1305")
+var ErrChacha20poly1305Failed = errors.New("encryption/decryption failed using ChaCha20-Poly1305")
