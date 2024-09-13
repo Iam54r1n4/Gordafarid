7b827d2a9c018b5ec63d34ab4f78456ef3b455a0
Sarina <iam54r1n40@proton.me>
2024-09-13 17:14:47 +0330

    Document utils.go

--- core/net/utils/utils.go
+++ core/net/utils/utils.go
@@ -10,6 +10,17 @@ import (
 	"github.com/Iam54r1n4/Gordafrid/internal/proxy_error"
 )
 
+// ReadWithContext reads data from a net.Conn with context support.
+// It allows for cancellation and timeout handling using the provided context.
+//
+// Parameters:
+//   - ctx: The context for cancellation and timeout control.
+//   - c: The net.Conn to read from.
+//   - buf: The buffer to read data into.
+//
+// Returns:
+//   - int: The number of bytes read.
+//   - error: Any error that occurred during the read operation or context cancellation.
 func ReadWithContext(ctx context.Context, c net.Conn, buf []byte) (int, error) {
 	readChan := make(chan struct {
 		n   int
@@ -36,6 +47,18 @@ func ReadWithContext(ctx context.Context, c net.Conn, buf []byte) (int, error) {
 	}
 }
 
+// DataTransfering transfers data between two network connections.
+// It uses io.Copy to efficiently copy data from the right connection to the left connection.
+//
+// Parameters:
+//   - wg: A pointer to a sync.WaitGroup, used to signal when the function has completed.
+//   - errChan: A channel to send any errors that occur during the data transfer.
+//   - left: The destination net.Conn where data will be written.
+//   - right: The source net.Conn from which data will be read.
+//
+// The function will decrement the WaitGroup counter when it completes.
+// If an error occurs during the data transfer, it will be sent to the errChan
+// wrapped with the proxy_error.ErrTransferError.
 func DataTransfering(wg *sync.WaitGroup, errChan chan error, left net.Conn, right net.Conn) {
 	defer wg.Done()
 	if _, err := io.Copy(left, right); err != nil {
