b3992ea63bb4d401a27906d805ae51218631f8a5
Sarina <iam54r1n40@proton.me>
2024-09-16 14:27:46 +0330

    Update errors

--- internal/proxy_error/proxy_error.go
+++ internal/proxy_error/proxy_error.go
@@ -30,22 +30,46 @@ var (
 
 // Socks5 errors
 var (
-	ErrSocks5HandshakeTimeout            = errors.New("SOCKS5 handshake timed out")
-	ErrSocks5HandshakeFailed             = errors.New("SOCKS5 handshake failed: protocol mismatch or authentication error")
-	ErrSocks5UnsupportedVersion          = errors.New("unsupported SOCKS5 version")
-	ErrSocks5UnableToReadVersion         = errors.New("unable to read SOCKS5 version")
-	ErrSocks5InvalidNMethodsValue        = errors.New("invalid SOCKS5 nmethods value")
-	ErrSocks5InvalidMethod               = errors.New("invalid SOCKS5 method")
-	ErrSocks5UnableToDiscardNMethods     = errors.New("unable to discard SOCKS5 nmethods")
-	ErrSocks5UnableToSendVersionResponse = errors.New("unable to send SOCKS5 version response(first response)") // First response
-	ErrSocks5UnableToReadRequest         = errors.New("unable to read SOCKS5 request")
+	// General errors
+	ErrSocks5HandshakeTimeout       = errors.New("SOCKS5 handshake timed out")
+	ErrSocks5HandshakeFailed        = errors.New("SOCKS5 handshake failed: protocol mismatch or authentication error")
+	ErrSocks5HeaderValidationFailed = errors.New("SOCKS5 header validation failed")
+	ErrSocks5UnableToReadRequest    = errors.New("unable to read SOCKS5 request")
+
+	// Validation errors
+	ErrSocks5ClientSideInitialGreetingFailed = errors.New("client side SOCKS5 initial greeting failed")
+	ErrSocks5ClientSideInitialGreetingEmpty  = errors.New("client side SOCKS5 initial greeting is empty")
+
+	// Version errors
+	ErrSocks5UnsupportedVersion  = errors.New("unsupported SOCKS5 version")
+	ErrSocks5UnableToReadVersion = errors.New("unable to read SOCKS5 version")
+
+	// Send errors
+	ErrSocks5UnableToSendVersionResponse             = errors.New("unable to send SOCKS5 version response(first response)")  // First response
+	ErrSocks5UnableToSendSuccessResponse             = errors.New("unable to send SOCKS5 success response(second response)") // Second response
+	ErrSocks5UnableToSendUserPassAuthSuccessResponse = errors.New("unable to send SOCKS5 username/password authentication success response")
+	ErrSocks5UnableToSendUserPassAuthFailedResponse  = errors.New("unable to send SOCKS5 username/password authentication failed response")
+
+	// CMD errors
 	ErrSocks5UnsupportedVersionOrCommand = errors.New("unsupported SOCKS5 version or command(in handshake request)")
-	ErrSocks5UnableToReadAddressType     = errors.New("unable to read address type")
-	ErrSocks5UnsupportedAddressType      = errors.New("unsupported SOCKS5 address type")
-	ErrSocks5UnableToReadIpv4            = errors.New("unable to read IPv4 address")
-	ErrSocks5UnableToReadIpv6            = errors.New("unable to read IPv6 address")
-	ErrSocks5UnableToReadDomain          = errors.New("unable to read domain name")
-	ErrSocks5UnableToReadPort            = errors.New("unable to read port")
-	ErrSocks5UnableToSendSuccessResponse = errors.New("unable to send success response(second response)") // Second response
-	ErrSocks5HeaderValidationFailed      = errors.New("SOCKS5 header validation failed")
+
+	// Read network addresses errors
+	ErrSocks5UnableToReadAddressType = errors.New("unable to read SOCKS5 address type")
+	ErrSocks5UnsupportedAddressType  = errors.New("unsupported SOCKS5 address type")
+	ErrSocks5UnableToReadIpv4        = errors.New("unable to read SOCKS5 IPv4 address")
+	ErrSocks5UnableToReadIpv6        = errors.New("unable to read SOCKS5 IPv6 address")
+	ErrSocks5UnableToReadDomain      = errors.New("unable to read SOCKS5 domain name")
+	ErrSocks5UnableToReadPort        = errors.New("unable to read SOCKS5 port")
+
+	// Authentication errors
+	ErrSocks5InvalidNMethodsValue = errors.New("invalid SOCKS5 nmethods value")
+	ErrSocks5InvalidMethod        = errors.New("invalid SOCKS5 method")
+
+	// Username/Password authentication mehtod negotiation errors
+	ErrSocks5UnableToReadUserPassAuthVersion        = errors.New("unable to read SOCKS5 username/password authenticaation version")
+	ErrSocks5UnsupportedUserPassAuthVersion         = errors.New("unsupported SOCKS5 username/password authentication version")
+	ErrSocks5UnableToReadUserPassAuthUsernameLength = errors.New("unable to read SOCKS5 username/password authentication username length")
+	ErrSocks5UnableToReadUserPassAuthUsername       = errors.New("unable to read SOCKS5 username/password authentication username")
+	ErrSocks5UnableToReadUserPassAuthPasswordLength = errors.New("unable to read SOCKS5 username/password authentication password length")
+	ErrSocks5UnableToReadUserPassAuthPassword       = errors.New("unable to read SOCKS5 username/password authentication password")
 )
