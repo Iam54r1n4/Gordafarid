5525702a471542773e0d864663192f653a616649
Sarina <iam54r1n40@proton.me>
2024-09-17 18:33:33 +0330

    Seperate server and client config loaders

---
+++ internal/config/client.go
@@ -0,0 +1,87 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Iam54r1n4/Gordafarid/core/crypto"
+)
+
+// clientAddr holds the configuration for the client
+type clientAddr struct {
+	Address string `toml:"address"` // The address for the client to connect to
+}
+
+// socks5credentialsConfig is a map of usernames to passwords
+type socks5credentialsConfig map[string]string
+
+type ClientConfig struct {
+	Server            serverAddr              `toml:"server"`
+	Client            clientAddr              `toml:"client"`
+	CryptoAlgorithm   string                  `toml:"cryptoAlgorithm"`
+	Account           Account                 `toml:"account"`
+	Timeout           timeoutConfig           `toml:"timeout"`
+	Socks5Credentials socks5credentialsConfig `toml:"socks5Credentials"`
+}
+
+func LoadClientConfig(path string) (*ClientConfig, error) {
+	if path == "" {
+		path = DefaultConfigFilePath
+	}
+	var config ClientConfig
+	var err error
+
+	if _, err = toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
+	if err = config.validate(); err != nil {
+		return nil, err
+	}
+	config.applyDefaultValues()
+	return &config, nil
+}
+
+func (cc *ClientConfig) IsSocks5AuthEnabled() bool {
+	return len(cc.Socks5Credentials) > 0
+}
+
+func (cc *ClientConfig) validate() error {
+	var missingFields []string
+
+	if len(cc.Server.Address) < 1 {
+		missingFields = append(missingFields, "server.address")
+	}
+	if len(cc.Client.Address) < 1 {
+		missingFields = append(missingFields, "client.address")
+	}
+	if len(cc.CryptoAlgorithm) < 1 {
+		missingFields = append(missingFields, "cryptoAlgorithm")
+	}
+	if len(cc.Account.Username) < 1 {
+		missingFields = append(missingFields, "account.username")
+	}
+	if len(cc.Account.Password) < 1 {
+		missingFields = append(missingFields, "account.password")
+	}
+	if len(missingFields) > 0 {
+		return fmt.Errorf("missing fields: %s", strings.Join(missingFields, ", "))
+	}
+	if err := crypto.IsCryptoSupported(cc.CryptoAlgorithm, cc.Account.Password); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func (cc *ClientConfig) applyDefaultValues() {
+	if cc.Timeout.DialTimeout == 0 {
+		cc.Timeout.DialTimeout = 10
+	}
+	if cc.Timeout.Socks5HandshakeTimeout == 0 {
+		cc.Timeout.Socks5HandshakeTimeout = 10
+	}
+	if cc.Timeout.GordafaridHandshakeTimeout == 0 {
+		cc.Timeout.GordafaridHandshakeTimeout = 10
+	}
+}
---
+++ internal/config/server.go
@@ -0,0 +1,79 @@
+package config
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/BurntSushi/toml"
+	"github.com/Iam54r1n4/Gordafarid/core/crypto"
+)
+
+// serverAddr holds the configuration for the server
+type serverAddr struct {
+	Address string `toml:"address"` // The address for the server to listen on
+}
+
+type ServerConfig struct {
+	Server          serverAddr    `toml:"server"`
+	CryptoAlgorithm string        `toml:"cryptoAlgorithm"`
+	Credentials     []Account     `toml:"credentials"`
+	Timeout         timeoutConfig `toml:"timeout"`
+}
+
+func LoadServerConfig(path string) (*ServerConfig, error) {
+	if path == "" {
+		path = DefaultConfigFilePath
+	}
+	var config ServerConfig
+	var err error
+
+	if _, err = toml.DecodeFile(path, &config); err != nil {
+		return nil, err
+	}
+	if err = config.validate(); err != nil {
+		return nil, err
+	}
+	config.applyDefaultValues()
+	return &config, nil
+}
+
+func (sc *ServerConfig) validate() error {
+	var missingFields []string
+	if len(sc.Server.Address) < 1 {
+		missingFields = append(missingFields, "server.address")
+	}
+	if len(sc.CryptoAlgorithm) < 1 {
+		missingFields = append(missingFields, "cryptoAlgorithm")
+	}
+	if len(sc.Credentials) < 1 {
+		missingFields = append(missingFields, "credentials")
+	}
+	if len(missingFields) > 0 {
+		return fmt.Errorf("missing fields: %s", strings.Join(missingFields, ", "))
+	}
+
+	for i, cred := range sc.Credentials {
+		if len(cred.Username) < 1 {
+			return fmt.Errorf("element at index %d has empty username in credentials", i)
+		}
+		if len(cred.Password) < 1 {
+			return fmt.Errorf("element at index %d has empty password in credentials", i)
+		}
+		if err := crypto.IsCryptoSupported(sc.CryptoAlgorithm, cred.Password); err != nil {
+			return fmt.Errorf("element at index %d has invalid password in credentials, the required length is %d", i, crypto.GetAlgorithmKeySize(sc.CryptoAlgorithm))
+		}
+	}
+	return nil
+}
+
+func (sc *ServerConfig) applyDefaultValues() {
+	if sc.Timeout.DialTimeout == 0 {
+		sc.Timeout.DialTimeout = 10
+	}
+	if sc.Timeout.Socks5HandshakeTimeout == 0 {
+		sc.Timeout.Socks5HandshakeTimeout = 10
+	}
+	if sc.Timeout.GordafaridHandshakeTimeout == 0 {
+		sc.Timeout.GordafaridHandshakeTimeout = 10
+	}
+}
