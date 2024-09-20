d78865a2b77cb704fa475e06b9426001f89156fb
Sarina <iam54r1n40@proton.me>
2024-09-20 19:54:30 +0330

    Fix typo in gordafarid/header.go

--- core/net/protocol/gordafarid/header.go
+++ core/net/protocol/gordafarid/header.go
@@ -1,22 +1,22 @@
-// Package gordafarid implements the Gordafarid protocol for network communication.
+// Package gordafarid implements the Gordafarid protocol, a custom network protocol for secure communication.
 package gordafarid
 
 import "github.com/Iam54r1n4/Gordafarid/core/net/protocol"
 
 // greetingHeader represents the header structure for the greeting message in the Gordafarid protocol.
 type greetingHeader struct {
-	protocol.BasicHeaader                // Embedded BasicHeader from the protocol package
-	hash                  [HashSize]byte // Hash value used for authentication or integrity checking
+	protocol.BasicHeader                // Embedded BasicHeader from the protocol package
+	hash                 [HashSize]byte // Hash value used for authentication or integrity checking
 }
 
 // Size returns the total size of the greeting header in bytes.
 func (gh *greetingHeader) Size() int {
-	return gh.BasicHeaader.Size() + HashSize
+	return gh.BasicHeader.Size() + HashSize
 }
 
 // Bytes serializes the greeting header into a byte slice.
 func (gh *greetingHeader) Bytes() []byte {
-	return append(gh.BasicHeaader.Bytes(), gh.hash[:]...)
+	return append(gh.BasicHeader.Bytes(), gh.hash[:]...)
 }
 
 // requestHeader represents the header structure for request messages in the Gordafarid protocol.
@@ -38,15 +38,15 @@ func (rh *requestHeader) Bytes() []byte {
 type replyHeader struct {
 	Version byte                   // Protocol version
 	Status  byte                   // Status code of the reply
-	bind    protocol.AddressHeader // Address information for binding
+	Bind    protocol.AddressHeader // Address information for binding
 }
 
 // Size returns the total size of the reply header in bytes.
 func (rh *replyHeader) Size() int {
-	return 1 + 1 + rh.bind.Size() // 1 byte for Version, 1 byte for Status, plus the size of the AddressHeader
+	return 1 + 1 + rh.Bind.Size() // 1 byte for Version, 1 byte for Status, plus the size of the AddressHeader
 }
 
 // Bytes serializes the reply header into a byte slice.
 func (rh *replyHeader) Bytes() []byte {
-	return append([]byte{rh.Version, rh.Status}, rh.bind.Bytes()...)
+	return append([]byte{rh.Version, rh.Status}, rh.Bind.Bytes()...)
 }
