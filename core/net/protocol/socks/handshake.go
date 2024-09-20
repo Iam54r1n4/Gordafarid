d6170e94c2677d3ed7750582a7e4750315c67c11
Sarina <iam54r1n40@proton.me>
2024-09-20 20:18:28 +0330

    Update socks/handshake.go

--- core/net/protocol/socks/handshake.go
+++ core/net/protocol/socks/handshake.go
@@ -4,84 +4,84 @@ import (
 	"context"
 )
 
-// Handshake performs the SOCKS5 handshake process
-// This function follows the SOCKS5 protocol as defined in RFC 1928 and RFC 1929
-// https://www.ietf.org/rfc/rfc1928.txt
-// https://www.ietf.org/rfc/rfc1929.txt
-// It handles the initial greeting, method selection, authentication (if required), and the SOCKS5 request
+/*
+Socks5 handshake process:
+https://www.ietf.org/rfc/rfc1928.txt
+https://www.ietf.org/rfc/rfc1929.txt
 
-// Client -> Server: Initial Greeting
-// +----+----------+----------+
-// |VER | NMETHODS | METHODS  |
-// +----+----------+----------+
-// | 1  |    1     | 1 to 255 |
-// +----+----------+----------+
-//
-// VER: SOCKS version (0x05 for SOCKS5)
-// NMETHODS: Number of authentication methods supported
-// METHODS: Authentication methods supported
+Client -> Server: Initial Greeting
++----+----------+----------+
+|VER | NMETHODS | METHODS  |
++----+----------+----------+
+| 1  |    1     | 1 to 255 |
++----+----------+----------+
 
-// Server -> Client: Method Selection
-// +----+--------+
-// |VER | METHOD |
-// +----+--------+
-// | 1  |   1    |
-// +----+--------+
-//
-// VER: SOCKS version (0x05 for SOCKS5)
-// METHOD: Selected authentication method
+VER: SOCKS version (0x05 for SOCKS5)
+NMETHODS: Number of authentication methods supported
+METHODS: Authentication methods supported
 
-// If USERNAME/PASSWORD authentication is selected:
-// Client -> Server: Username/Password Authentication
-// +----+------+----------+------+----------+
-// |VER | ULEN |  UNAME   | PLEN |  PASSWD  |
-// +----+------+----------+------+----------+
-// | 1  |  1   | 1 to 255 |  1   | 1 to 255 |
-// +----+------+----------+------+----------+
-//
-// VER: Authentication version (0x01 for username/password auth)
-// ULEN: Username length
-// UNAME: Username
-// PLEN: Password length
-// PASSWD: Password
+Server -> Client: Method Selection
++----+--------+
+|VER | METHOD |
++----+--------+
+| 1  |   1    |
++----+--------+
 
-// Server -> Client: Authentication Response
-// +----+--------+
-// |VER | STATUS |
-// +----+--------+
-// | 1  |   1    |
-// +----+--------+
-//
-// VER: Authentication version (0x01 for username/password auth)
-// STATUS: Authentication status (0x00 for success, 0x01 for failure)
+VER: SOCKS version (0x05 for SOCKS5)
+METHOD: Selected authentication method
 
-// Client -> Server: SOCKS5 Request
-// +----+-----+-------+------+----------+----------+
-// |VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
-// +----+-----+-------+------+----------+----------+
-// | 1  |  1  | X'00' |  1   | Variable |    2     |
-// +----+-----+-------+------+----------+----------+
-//
-// VER: SOCKS version (0x05 for SOCKS5)
-// CMD: Command (0x01 for CONNECT, 0x02 for BIND, 0x03 for UDP ASSOCIATE)
-// RSV: Reserved byte, must be 0x00
-// ATYP: Address type (0x01 for IPv4, 0x03 for Domain, 0x04 for IPv6)
-// DST.ADDR: Destination address
-// DST.PORT: Destination port
+If USERNAME/PASSWORD authentication is selected:
+Client -> Server: Username/Password Authentication
++----+------+----------+------+----------+
+|VER | ULEN |  UNAME   | PLEN |  PASSWD  |
++----+------+----------+------+----------+
+| 1  |  1   | 1 to 255 |  1   | 1 to 255 |
++----+------+----------+------+----------+
 
-// Server -> Client: SOCKS5 Reply
-// +----+-----+-------+------+----------+----------+
-// |VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
-// +----+-----+-------+------+----------+----------+
-// | 1  |  1  | X'00' |  1   | Variable |    2     |
-// +----+-----+-------+------+----------+----------+
-//
-// VER: SOCKS version (0x05 for SOCKS5)
-// REP: Reply field (0x00 for success, other values for various errors)
-// RSV: Reserved byte, must be 0x00
-// ATYP: Address type (0x01 for IPv4, 0x03 for Domain, 0x04 for IPv6)
-// BND.ADDR: Server bound address
-// BND.PORT: Server bound port
+VER: Authentication version (0x01 for username/password auth)
+ULEN: Username length
+UNAME: Username
+PLEN: Password length
+PASSWD: Password
+
+Server -> Client: Authentication Response
++----+--------+
+|VER | STATUS |
++----+--------+
+| 1  |   1    |
++----+--------+
+
+VER: Authentication version (0x01 for username/password auth)
+STATUS: Authentication status (0x00 for success, 0x01 for failure)
+
+Client -> Server: SOCKS5 Request
++----+-----+-------+------+----------+----------+
+|VER | CMD |  RSV  | ATYP | DST.ADDR | DST.PORT |
++----+-----+-------+------+----------+----------+
+| 1  |  1  | X'00' |  1   | Variable |    2     |
++----+-----+-------+------+----------+----------+
+
+VER: SOCKS version (0x05 for SOCKS5)
+CMD: Command (0x01 for CONNECT, 0x02 for BIND, 0x03 for UDP ASSOCIATE)
+RSV: Reserved byte, must be 0x00
+ATYP: Address type (0x01 for IPv4, 0x03 for Domain, 0x04 for IPv6)
+DST.ADDR: Destination address
+DST.PORT: Destination port
+
+Server -> Client: SOCKS5 Reply
++----+-----+-------+------+----------+----------+
+|VER | REP |  RSV  | ATYP | BND.ADDR | BND.PORT |
++----+-----+-------+------+----------+----------+
+| 1  |  1  | X'00' |  1   | Variable |    2     |
++----+-----+-------+------+----------+----------+
+
+VER: SOCKS version (0x05 for SOCKS5)
+REP: Reply field (0x00 for success, other values for various errors)
+RSV: Reserved byte, must be 0x00
+ATYP: Address type (0x01 for IPv4, 0x03 for Domain, 0x04 for IPv6)
+BND.ADDR: Server bound address
+BND.PORT: Server bound port
+*/
 
 // Handshake initiates the SOCKS handshake process.
 // It's a convenience method that calls HandshakeContext with a background context.
