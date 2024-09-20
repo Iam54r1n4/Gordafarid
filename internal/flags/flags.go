51fcefb0049dfbcd406ceadb51cc064e31357a6a
Sarina <iam54r1n40@proton.me>
2024-09-20 19:08:06 +0330

    Document flags.go

--- internal/flags/flags.go
+++ internal/flags/flags.go
@@ -6,7 +6,10 @@ import (
 
 // The program's flags
 var (
-	CfgPathFlag  string
+	// CfgPathFlag is the path to the configuration file
+	CfgPathFlag string
+
+	// HashSaltFlag is the hash salt for Gordafarid authentication
 	HashSaltFlag string
 )
 
@@ -15,12 +18,18 @@ const (
 	// DefaultConfigFilePath is the default path for the configuration file
 	defaultConfigFilePath = "./config.toml"
 
-	// Hash salt for the Gordafarid authentication
+	// DefaultHashSalt is the default hash salt for the Gordafarid authentication
 	defaultHashSalt = "ZZA"
 )
 
+// init initializes the command-line flags
 func init() {
+	// Set up the configuration file path flag
 	flag.StringVar(&CfgPathFlag, "config", defaultConfigFilePath, "path to config file")
+
+	// Set up the hash salt flag for Gordafarid authentication
 	flag.StringVar(&HashSaltFlag, "hashSalt", defaultHashSalt, "hash salt for Gordafarid authentication")
+
+	// Parse the command-line flags
 	flag.Parse()
 }
