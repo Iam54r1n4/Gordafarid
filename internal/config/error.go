fa0e80251fc024fb2c6e0cf5f76ffff3c5f9cc6f
Sarina <iam54r1n40@proton.me>
2024-09-20 19:11:19 +0330

    Update config/error.go

--- internal/config/error.go
+++ internal/config/error.go
@@ -4,7 +4,5 @@ import "errors"
 
 var (
 	errInvalidConfigFile      = errors.New("invalid config file")
-	errCryptoAlgorithmEmpty   = errors.New("crypto.algorithm is empty")
-	errCryptoInitFailed       = errors.New("the crypto initialization failed")
 	errEmptyServerCredentials = errors.New("server.credentials is empty")
 )
